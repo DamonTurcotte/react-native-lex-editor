3d3b55e69f72a53ca6c83126700deb74903e8a6a
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-10-20 04:12:35 -0600

    refactor: update RichText component to support development mode
    The RichText component in the lex library has been updated to support development mode. The component now accepts a new prop called _DEV_ which, when set to true, will load the editor from a local development server. This allows for easier testing and debugging during development.

--- libraries/lex/src/editor/RichText.tsx
+++ libraries/lex/src/editor/RichText.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { WebView } from 'react-native-webview';
+import {
+  WebView,
+  type WebViewProps,
+} from 'react-native-webview';
 
 import { htmlString } from 'react-native-lex-editor-web';
 
-const RichText = () => {
-  return <WebView source={{ html: htmlString }} />;
-};
+export interface RichTextProps extends WebViewProps {
+  _DEV_?: boolean;
+}
 
-export {
-  RichText,
+export const RichText = ({
+  _DEV_,
+  ...props
+}: RichTextProps ): React.JSX.Element => {
+
+  return (
+    <WebView
+      {...props}
+      source={props.source ? props.source : _DEV_ ? { uri: 'http://10.0.0.219:4200' } : { html: htmlString }}
+    />
+  );
 };
