d28d8b40babf245d35c45f556dc7fc7c4bdd5374
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-10-24 04:05:54 -0600

    feat: Add App component with Lex Editor integration

---
+++ example/src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { RichText } from 'react-native-lex-editor';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container} edges={['top', 'bottom']}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Lex Editor</Text>
+        <Pressable onPress={() => console.log('Clicked')}>
+          <Text>Resize</Text>
+        </Pressable>
+      </View>
+      <RichText
+        _DEV_={true}
+      />
+    </SafeAreaView>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  header: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
