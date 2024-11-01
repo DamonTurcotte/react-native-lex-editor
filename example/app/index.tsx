7266b3739318b889e6321dd2856995bc10561a2b
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-11-01 05:09:46 -0600

    feat: test lexical command feature

--- example/app/index.tsx
+++ example/app/index.tsx
@@ -1,22 +1,30 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import { RichText, useEditorBridge } from "react-native-lex-editor";
+import { StyleSheet, View, Button } from "react-native";
+import { LexEditor, useLexical } from "react-native-lex-editor";
 
 const initialState = '{"root": {"children": [{"children": [{"detail": 0,"format": 0,"mode": "normal","style": "","text": "Hello, Lex!","type": "text","version": 1}],"direction": "ltr","format": "","indent": 0,"type": "paragraph","version": 1,"textFormat": 0,"textStyle": ""}],"direction": "ltr","format": "","indent": 0,"type": "root","version": 1}}';
 
 const HomeView = () => {
-  const editor = useEditorBridge({
+  const editor = useLexical({
     _DEV: true,
     initialState,
     onChangeState: (state) => {
       console.log('onChangeState:', state);
-    }
+    },
   });
+
+  const sendTestCommand = () => {
+    editor.sendCommand({ type: 'TEST_COMMAND' });
+  }
 
   return (
     <View style={styles.container}>
-      <RichText
+      <LexEditor
         editor={editor}
+      />
+      <Button
+        title="Send Test Command"
+        onPress={sendTestCommand}
       />
     </View>
   )
